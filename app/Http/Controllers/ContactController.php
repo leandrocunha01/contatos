703c3e106739d4ddc5252b1ae804eec58a0961be
Leandro Cunha <leandrocunha01@gmail.com>
2022-12-01 19:29:02 -0300

    Adicione controle para contatos
    Adiciona controle e rotas para contatos

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContactRequest;
+use App\Models\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return Contact::with('address')->get();
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email|unique:contacts',
+            'cpf' => 'required:unique:contacts'
+        ];
+        $validator = $this->validator($request, $rules);
+        if ($validator->fails()) {
+            $response = ['success' => false];
+            $response['response'] = $validator->messages();
+            return $response;
+        } else {
+            return Contact::create($request->all());;
+        }
+    }
+
+    public function update(Request $request){
+        $rules = [
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email',
+            'cpf' => 'required'
+        ];
+        $validator = $this->validator($request, $rules);
+
+        $response = ['success' => false];
+        if ($validator->fails()) {
+            $response['response'] = $validator->messages();
+            return $response;
+        } else {
+            $contact = Contact::find($request->id);
+        }
+        if(!$contact){
+            return ['success' => false, 'response' =>  'Contato não encontrado'];
+        }else{
+            $contact->update($request->all());
+        }
+        return $contact;
+    }
+
+    public function validator(Request $request, $rules){
+        $response = array('response' => '', 'success' => false);
+
+        $validator = Validator::make($request->all(), $rules);
+
+        return $validator;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/contacts', 'App\Http\Controllers\ContactController@index');
+Route::post('/contacts', 'App\Http\Controllers\ContactController@store');
+Route::put('/contacts/{id}', 'App\Http\Controllers\ContactController@update');
+
