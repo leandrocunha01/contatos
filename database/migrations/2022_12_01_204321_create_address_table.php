7737715fe191e666eb7acde4ea09a39dcd3cf29b
Leandro Cunha <leandrocunha01@gmail.com>
2022-12-01 19:26:46 -0300

    Adicione migrations
    Adiciona schema para tabelas de contato e endereços

---
+++ database/migrations/2022_12_01_204321_create_address_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('contact_id')->unsigned();
+            $table->foreign('contact_id')->references('id')->on('contacts');
+            $table->string('address');
+            $table->string('complement')->nullable();
+            $table->string('district');
+            $table->string('city');
+            $table->string('state');
+            $table->string('cep');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+};
