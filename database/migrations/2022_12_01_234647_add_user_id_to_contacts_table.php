64e7f8b01e31ee0417304787493a25eed6f778f4
Leandro Cunha <leandrocunha01@gmail.com>
2022-12-01 21:10:46 -0300

    Mude tipo de users e adicione relacionamento
    Adiciona relacionamento com contatos e muda tipo da tabela do id de
    usuários para int para que funcione com foreing

--- database/migrations/2022_12_01_234647_add_user_id_to_contacts_table.php
+++ database/migrations/2022_12_01_234647_add_user_id_to_contacts_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->nullable()->after('id');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +26,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
     }
 };
